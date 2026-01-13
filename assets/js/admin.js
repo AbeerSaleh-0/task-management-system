cb8994b390a577668574005fc5d26377f73871c4
AbeerSaleh-0 <112480455+AbeerSaleh-0@users.noreply.github.com>
2026-01-13 11:57:32 +0300

    overdue tasks

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -65,7 +65,7 @@ function updateOverview() {
    // حساب المهام المتأخرة
   const overdueTasks = allTasks.filter(t => {
     if (t.status === 'completed') return false;
-    const dueDate = new Date(t.due_Date);
+    const dueDate = t.due_Date;
     console.log(dueDate);
     const today = new Date();
     console.log(today);
