ca5a4acfe4a8d2c8c5d0856af779589513c99c86
AbeerSaleh-0 <112480455+AbeerSaleh-0@users.noreply.github.com>
2026-01-25 16:34:55 +0300

    url

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors({
   origin: [
-    'https://taskrsg.cloud/', // URL
+    'https://taskrsg.cloud', // URL
     'http://localhost:3000', // للتطوير المحلي
     'http://127.0.0.1:5500'  // Live Server
   ],
